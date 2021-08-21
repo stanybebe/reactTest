489fc34267dc3a524a891b52bbcd0f5d908f9296
stanybebe <tristan.e.whitehill@gmail.com>
2021-08-21 15:39:16 -0400

    Update ReadCSV.js

--- ReadCSV.js
+++ ReadCSV.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { readRemoteFile } from 'react-papaparse';
 
+
 export default class ReadRemoteFile extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = { 
+         checking:false,
+         objs: Array()
+      };
+    }
     handleClick = () => {
     readRemoteFile('https://raw.githubusercontent.com/stanybebe/stanybebe.github.io/master/AmphiBIO_v1.csv', {
       complete: (results) => {
@@ -12,33 +19,38 @@ export default class ReadRemoteFile extends Component {
         let objs = val.map(array => {
             let obj ={};
             keys.forEach((key, i) => obj[key] = array[i]);
-           
             return obj;
 
         });
         console.log('Results:', objs);
-        objs.map((ob, key) =>
-        <div key={key}>
-        <ul key={ob.id}> 
-          <li >{ob.Family}</li>
-          <li >{ob.id}</li>
-        </ul>
-        </div>
-        );
-        
+        this.setState({
+        objs: objs
+        });
+        this.renderData();
       }, 
       
     });
 
-   
-
   };  
+
+  renderData() {
+    const formattedObs = this.state.objs.map((ob) =>{
+        <ul key={ob.id}>
+        <li>{ob.Family}</li>
+        </ul>
+    })
+    return (
+    <div>{formattedObs}</div>
+
+    );
+  }
 
   render() {
     return (
      <div> 
      <button onClick={this.handleClick}>readRemoteFile</button>
-     {readRemoteFile.displayObs}
+     {this.renderData()}
+
     </div>)
   }
 }
